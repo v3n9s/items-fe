7d9b9f1c20b44035f610e533b2b9b4e4e8f48de2
Venyamin Shvets <venyashvets@gmail.com>
2022-08-29 13:53:44 +0300

    feat: create auth interfaces

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,6 +1,17 @@
+import { Tokens } from './tokens';
+
 export interface User {
   id: number;
   name: string;
   isAdmin: boolean;
   isBlocked: boolean;
+}
+
+export interface UserAuthData {
+  name: string;
+  password: string;
+}
+
+export interface UserAuthState extends Tokens {
+  user: User;
 }
