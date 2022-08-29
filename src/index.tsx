838bb4c81a7d573c80051e7ff508d4541553131a
Venyamin Shvets <venyashvets@gmail.com>
2022-08-29 14:31:50 +0300

    feat: wrap app with router

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App';
 import { store } from './store';
 import './i18n';
@@ -9,7 +10,9 @@ import './i18n';
 const root = ReactDOM.createRoot(document.querySelector('#root')!);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 );
