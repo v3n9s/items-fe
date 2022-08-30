0a8221b67ea919c895766275a9e9b6029e297126
Venyamin Shvets <venyashvets@gmail.com>
2022-08-30 15:45:01 +0300

    feat: add some translations

--- src/i18n/locales/be.ts
+++ src/i18n/locales/be.ts
@@ -1,1 +1,25 @@
-export default {};
+export default {
+  translation: {
+    auth: {
+      login: 'Увайсці',
+      register: 'Зарэгістравацца',
+      name: 'Імя',
+      password: 'Пароль',
+      repeatPassword: 'Паўтарыце пароль',
+      passwordsMustMatch: 'Паролі павінны супадаць'
+    },
+    nav: {
+      menu: 'Меню',
+      close: 'Зачыніць'
+    },
+    validation: {
+      required: 'Поле "{{field}}" з\'яўляецца абавязковым',
+      min: 'Поле "{{field}}" павінна быць ня менш за {{min}} {{chars}}'
+    },
+    utils: {
+      characters_one: 'сімвал',
+      characters_few: 'сімвала',
+      characters_many: 'сімвалаў'
+    }
+  }
+};
--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,1 +1,24 @@
-export default {};
+export default {
+  translation: {
+    auth: {
+      login: 'Login',
+      register: 'Register',
+      name: 'Name',
+      password: 'Password',
+      repeatPassword: 'Repeat password',
+      passwordsMustMatch: 'Passwords must match'
+    },
+    nav: {
+      menu: 'Menu',
+      close: 'Close'
+    },
+    validation: {
+      required: 'Field "{{field}}" is required',
+      min: 'Field "{{field}}" must be at least {{min}} {{chars}} long'
+    },
+    utils: {
+      characters_one: 'character',
+      characters_other: 'characters'
+    }
+  }
+};
