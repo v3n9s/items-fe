6c4e507da60ed7aae9f1c3ad81e3b2135124bf5c
Venyamin Shvets <venyashvets@gmail.com>
2022-08-30 15:49:03 +0300

    feat: translate validation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,23 @@
-import i18next from 'i18next';
+import i18next, { t } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en';
 import be from './locales/be';
+import { setLocale } from 'yup';
+
+i18next.on('languageChanged', () => {
+  setLocale({
+    mixed: {
+      required: ({ path }) => t('validation.required', { field: t(`auth.${path}`) })
+    },
+    string: {
+      min: ({ path, min }) => t('validation.min', {
+        field: t(`auth.${path}`),
+        min,
+        chars: t('utils.characters', { count: min })
+      })
+    }
+  })
+});
 
 i18next
   .use(initReactI18next)
