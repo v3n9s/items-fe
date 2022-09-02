7205b5d70446df310682c5834c55c7da645434a2
Venyamin Shvets <venyashvets@gmail.com>
2022-09-02 16:09:02 +0300

    feat: set access token on request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import config from '../config';
+import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: config.BACKEND_URL
+  baseUrl: config.BACKEND_URL,
+  prepareHeaders: (headers, api) => {
+    const { accessToken } = (api.getState() as RootState).auth;
+    if (accessToken) {
+      headers.set('authorization', `Bearer ${accessToken}`);
+    }
+    return headers;
+  }
 });
 
 export const emptyBaseApi = createApi({
